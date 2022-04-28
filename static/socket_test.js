a226914222b7723dfef05891ee9da757f5968d41
Shane Hutter <shane.hutter@serversaustralia.com.au>
2022-04-28 16:55:01 +1000

    Create a command line notification system

--- static/socket_test.js
+++ static/socket_test.js
@@ -1,3 +1,5 @@
+let notify_perm;
+
 if( socket ){
     socket.on(
         "foobar",
@@ -8,4 +10,28 @@ if( socket ){
     );
 
     socket.emit( "foobar" , "init" );
+
+    socket.on(
+        "notify",
+        function( message ){
+            if( Notification ){
+                notify_perm = Notification.permission
+                console.log( notify_perm )
+                if( notify_perm = "default" ){
+                    Notification.requestPermission(
+                        function( permission ){
+                            console.log( permission )
+                            notify_perm = permission
+                            if( notify_perm == "granted" ){
+                                console.log( notify_perm )
+                            }
+                        }
+                    )
+                } else if( notify_perm = "granted" ) {
+                    console.log( notify_perm )
+                }
+            }
+        }
+    )
+
 }
